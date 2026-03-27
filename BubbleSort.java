4d14d2f9a1b9b91dbda4b6d28107b68c7ea07196
Jibesh <jibeshsinha06@gmail.com>
2026-03-27 19:45:32 +0530

    bubble sort added

---
+++ BubbleSort.java
@@ -0,0 +1,25 @@
+// package DSA_IN_JAVA;
+
+public class BubbleSort {
+    public static void sorting(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 1, 4, 2, 9 };
+        sorting(arr);
+        for (int num : arr) {
+            System.out.print(num+ " ");
+        
+    }
+}
+}
